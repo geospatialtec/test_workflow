dfd675e2b8d85471bceded38e192fe4ab828dac3
jimgta <jim@geospatialtec.com>
2024-08-06 18:11:13 +0000

    updates

--- process.py
+++ process.py
@@ -8,4 +8,4 @@ for filename in os.listdir("asd/"):
       text.append(f.read())
 
 df = pd.DataFrame({'col1': text})
-df.to_csv("output.csv", index=False)
+df.to_csv("output/output.csv", sep=',', index=False)
