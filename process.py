a317f2d1559044898df09470616b8504b8925fc0
jimgta <jim@geospatialtec.com>
2024-08-06 18:18:33 +0000

    update to create output directory if it doesn't exist

--- process.py
+++ process.py
@@ -7,5 +7,9 @@ for filename in os.listdir("asd/"):
    with open(os.path.join("asd/", filename), 'r') as f: # open in readonly mode
       text.append(f.read())
 
+output_dir = "output/"
+if not os.path.exists(output_dir):
+   os.mkdir(output_dir)
+
 df = pd.DataFrame({'col1': text})
-df.to_csv("output/output.csv", sep=',', index=False)
+df.to_csv(os.path.join(output_dir, "output.csv"), sep=',', index=False)
